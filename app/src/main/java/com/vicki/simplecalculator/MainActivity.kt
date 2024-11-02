35fa60f519a47f8a019a5b2144d2dcaac78b9fa3
vwhwang <vwhwang38@gmail.com>
2024-11-02 15:44:40 -0700

    use remember

--- app/src/main/java/com/vicki/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/vicki/simplecalculator/MainActivity.kt
@@ -11,13 +11,11 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.vicki.simplecalculator.ui.theme.SimpleCalculatorTheme
 
@@ -37,19 +35,23 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Column(modifier = Modifier) {
-                        var display by remember { mutableStateOf("") }
-                        var num1: Double? by remember { mutableStateOf(null) }
-                        var num2: Double? by remember { mutableStateOf(null) }
-                        var operation by remember { mutableStateOf("") }
-                        var answer: Double? by remember { mutableStateOf(null) }
-
+                        var display by rememberSaveable { mutableStateOf("") }
+                        var num1: Double? by rememberSaveable { mutableStateOf(null) }
+                        var num2: Double? by rememberSaveable { mutableStateOf(null) }
+                        var operation by rememberSaveable { mutableStateOf("") }
+                        var answer: Double? by rememberSaveable { mutableStateOf(null) }
 
                         Text("This is a simple Calculator!")
                         Text(text = display.toString())
                         Text(text = answer.toString())
 
                         Row() {
-                            Button(onClick = { answer = null }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                num1 = null
+                                num2 = null
+                                display = ""
+                                answer = null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "back")
                             }
                             Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
@@ -58,53 +60,97 @@ class MainActivity : ComponentActivity() {
                             Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "%")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                operation = "/"
+                                display = appendInput(display, "/")
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "/")
                             }
                         }
                         Row() {
                             Button(onClick = {
-                                display = appendInput(display,"7")
+                                display = appendInput(display, "7")
                                 num1 = if (num1 == null) 7.0 else num1
                                 num2 = if (num1 != null) 7.0 else null
-                                             }, modifier = Modifier.size(80.dp)) {
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "7")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                display = appendInput(display, "8")
+                                num1 = if (num1 == null) 8.0 else num1
+                                num2 = if (num1 != null) 8.0 else null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "8")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                display = appendInput(display, "9")
+                                num1 = if (num1 == null) 9.0 else num1
+                                num2 = if (num1 != null) 9.0 else null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "9")
                             }
-                            Button(onClick = { operation = "*" }, modifier = Modifier.size(80.dp)) {
-                                Text(text = "x")
+                            Button(onClick = {
+                                operation = "*"
+                                display = appendInput(display, "*")
+                            }, modifier = Modifier.size(80.dp)) {
+                                Text(text = "*")
                             }
                         }
                         Row() {
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                display = appendInput(display, "4")
+                                num1 = if (num1 == null) 4.0 else num1
+                                num2 = if (num1 != null) 4.0 else null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "4")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                display = appendInput(display, "5")
+                                num1 = if (num1 == null) 5.0 else num1
+                                num2 = if (num1 != null) 5.0 else null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "5")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                display = appendInput(display, "6")
+                                num1 = if (num1 == null) 6.0 else num1
+                                num2 = if (num1 != null) 6.0 else null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "6")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                operation = "-"
+                                display = appendInput(display, "-")
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "-")
                             }
                         }
                         Row() {
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                display = appendInput(display, "1")
+                                num1 = if (num1 == null) 1.0 else num1
+                                num2 = if (num1 != null) 1.0 else null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "1")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                display = appendInput(display, "2")
+                                num1 = if (num1 == null) 2.0 else num1
+                                num2 = if (num1 != null) 2.0 else null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "2")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                display = appendInput(display, "3")
+                                num1 = if (num1 == null) 3.0 else num1
+                                num2 = if (num1 != null) 3.0 else null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "3")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                operation = "+"
+                                display = appendInput(display, "+")
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "+")
                             }
                         }
@@ -112,16 +158,24 @@ class MainActivity : ComponentActivity() {
                             Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "duck")
                             }
-                            Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
+                            Button(onClick = {
+                                display = appendInput(display, "0")
+                                num1 = if (num1 == null) 0.0 else num1
+                                num2 = if (num1 != null) 0.0 else null
+                            }, modifier = Modifier.size(80.dp)) {
                                 Text(text = "0")
                             }
                             Button(onClick = { /*TODO*/ }, modifier = Modifier.size(80.dp)) {
                                 Text(text = ".")
                             }
-                            Button(onClick = { answer = calculate(num1,num2,operation) }, modifier = Modifier.size(80.dp)) {
+                            Button(
+                                onClick = { answer = calculate(num1, num2, operation) },
+                                modifier = Modifier.size(80.dp)
+                            ) {
                                 Text(text = "=")
                             }
                         }
+
                     }
 
                 }
@@ -130,28 +184,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SimpleCalculatorTheme {
-        Greeting("Android")
-    }
-}
 
 private fun appendInput(string: String, newString: String): String {
     return string + newString
 }
 
-private fun calculate(input1: Double?, input2: Double?, operator: String) : Double? {
-    val result = when(operator) {
+private fun calculate(input1: Double?, input2: Double?, operator: String): Double? {
+    val result = when (operator) {
         "+" -> input1?.plus(input2!!)
         "-" -> input1?.minus(input2!!)
         "*" -> input1?.times(input2!!)
